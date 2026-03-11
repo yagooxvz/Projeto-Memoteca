cc4b448587ec92af0a2029f88663a959834c7a54
Yago Souza <yahosouza090@gmail.com>
2026-03-11 10:55:25 -0300

    Adiciona pensamentos ao meu mural

---
+++ js/api.js
@@ -0,0 +1,29 @@
+const api = {
+    async buscarPensamentos() {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos');
+            return await response.json();
+        } catch {
+            alert('Erro ao buscar pensamentos');
+            throw error
+        }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(pensamento),
+            });
+            return await response.json();
+        } catch {
+            alert('Erro ao buscar pensamentos');
+            throw error;
+        }
+    },
+};
+
+export default api;
